ea50e104b80b74759787583a0e970d3424bba3b7
alexandriarosse <amr3499@g.rit.edu>
2021-04-29 11:30:57 -0400

    fixed

--- tweets_team2/tweetsAway.py
+++ tweets_team2/tweetsAway.py
@@ -9,7 +9,7 @@ import time
 import base64
 import os
 
-blue2url = 'http://10.0.2.4:5000/user/addtweet'
+blue2url = 'http://10.0.2.4:5000/tweet/add-tweet'
 
 # Our Users
 redTeamUsers = ['dlennon','mjvl','wDestler','hTubman123','JaneDoe','jbarrett01','johncena','lskywalker']
@@ -24,20 +24,13 @@ for u in data2['following']:
     if (u['username']) not in redTeamUsers:
         blue2.append(u['id'])
 
-# Loop through a folder of potato pics and encode them
-#potatoes = []
-#for filename in os.listdir('/home/alexandriarosse/Desktop/tweets/potatoez'):
-#    fullpath = os.path.join('/home/alexandriarosse/Desktop/tweets/potatoez/', filename)
-#    image = open(fullpath, 'rb')
-#    image_read = image.read()
-#    image_64 = base64.encodebytes(image_read)
-#    potatoes.append(image_64)
 
 # Words
 words = open('potato.txt')
 content = words.readlines()
 
 counter = 0
+
 # Go until the user interrupts
 #
 # Loop through all blue1 and blue2 users sending a tweet of
@@ -46,15 +39,9 @@ counter = 0
 # Will wait X seconds after each request to not DoS the website,
 # just be to annoying
 while(1):
-#    for u2 in blue2:
-#       req = {"userId" : (f"{u1}"), "text" : potatoes[counter % len(potatoes)]}
-#        res = requests.post(blue2url, json = req)
-#        print(res)
-#        counter += 1
-#        time.sleep(10)
-    for u2 in blue2:
-        req = {"userId" : (f"{u2}"), "text" : content[counter2 % len(content)]}
+    for u in blue2:
+        req = {"userId" : (f"{u}"), "text" : content[counter % len(content)]}
         res = requests.post(blue2url, json = req)
-        print(counter2 , res)
-        counter2 += 1
+        print(counter , res)
+        counter += 1
         time.sleep(10)
